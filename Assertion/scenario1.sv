a8c7bb7390ae51d7defa0064579c1acc1d2e33b1
Rupali3302 <rupalitirumalasetty@gmail.com>
2025-09-10 12:56:49 +0530

    scenario1.sv

---
+++ Assertion/scenario1.sv
@@ -0,0 +1,31 @@
+//An assertion to detect B is high,then 2 cycles before signal A must be high
+module assertion;
+  bit a,b;
+  bit clk;
+  initial 
+    begin
+      a=0;
+      b=0;
+      clk=0;
+    end
+  
+  always #5clk=~clk;
+  always@(negedge clk)
+    begin
+      a=$random%2;
+      b=$random%2;
+    end
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+      #100 $finish;
+    end
+  property p1;
+    @(posedge clk)a##2 b;
+  endproperty
+  a1:assert property (p1)
+    $info("Assertion pass at time %0t and value of a =%0b,b=%0b",$time,a,b);
+    else
+      $error("Assertion fail at time %0t and value of a =%0b,b=%0b",$time,a,b);
+endmodule
